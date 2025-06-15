3a15265fafdccddac766a7a48e0cb09f4f4c9bbf
ShreyanshVishwakarma <shreyanshvishwakarma@outlook.in>
2025-06-15 14:41:39 +0530

    added shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+//import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>hello world !</div>
+    <Button>This</Button>
   );
 }
